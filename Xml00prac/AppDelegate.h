d51ce4fa2720e8e75fad8bd1f067257fa86db530
Student-002 <Student-002@student-k-002.local>
2017-03-09 11:51:56 +0530

    Initial Commit

---
+++ Xml00prac/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Xml00prac
+//
+//  Created by Student-002 on 09/03/17.
+//  Copyright © 2017 Felix-its-com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Xml00prac/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Xml00prac
+//
+//  Created by Student-002 on 09/03/17.
+//  Copyright © 2017 Felix-its-com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
